89f4ccdce1ab551779e87c08e4392ff90afb5a7d
Timka09 <tymofij.i.naumenko@gmail.com>
2024-12-14 16:19:59 +0200

    дз

--- javascript.js
+++ javascript.js
@@ -1,10 +1,31 @@
-function makePrompt(callback) {
-    prompt(alert("Hello World!"));
-}
-function helloWorld(callback) {
-  while (true) {
-      callback(alert("Hello World!"));
-      break
+// Task 4
+const arr = [2, 3, 4, 5];
+function applyCallbackToEachElement(arr, callback) {
+  let newArr = [] 
+  for (let i = 0; i < arr.length; i++){
+    newArr.push(callback(arr[i]))
   }
+  return newArr
 }
-console.log(helloWorld(callback()));
+
+function squareCallback(arr) {
+  return arr * arr
+}
+
+console.log(applyCallbackToEachElement(arr,squareCallback))
+
+
+// Task 5
+
+function calculateDiscountedPrice(price, discount, callback) {
+  const calculation = (price / 100 * discount)
+  const calculationSale = price - calculation
+  return callback(calculationSale)
+}
+
+function showDiscountedPrice(calculationSale) {
+  let returnMessege = `Ваша дисконтна ціна дорівнює ${calculationSale}`
+  return returnMessege
+}
+
+console.log(calculateDiscountedPrice(100,10,showDiscountedPrice))
