2d465c3d19f74f584787edff3e29469395d8b71c
Timka09 <tymofij.i.naumenko@gmail.com>
2024-12-21 16:52:36 +0200

    dz

--- javascript.js
+++ javascript.js
@@ -1,3 +1,42 @@
+// Task 1
+function greatings() {
+  const saveClick = confirm("confirm me")
+  if (saveClick === true) {
+    alert("hello world")
+  } else {
+    alert("plese try again")
+  }
+}
+greatings()
+// Task 2
+const randomNumber = Math.floor(Math.random() * 100)
+console.log(randomNumber)
+const personAnswer = Number(prompt("guess number from 1 to 100"))
+
+function checkRightAnswer() {
+    if (personAnswer === randomNumber) {
+        alert("U won")
+    } else {
+        alert("Try again")
+    }
+
+}
+checkRightAnswer()
+// Task 3
+let res = 0;
+function infinityClick() {
+  while (true) {
+    const plusClick = confirm("Click me");
+    if (plusClick) {
+      res++;
+      console.log(`U clicked ${res}`);
+    } else {
+      console.log("click me again");
+      break;
+    }
+  }
+}
+infinityClick();
 // Task 4
 const arr = [2, 3, 4, 5];
 function applyCallbackToEachElement(arr, callback) {
